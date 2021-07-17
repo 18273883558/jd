f39f2dda9ae766a211afd357246b0265d5f8bb02
liu <1976898266@qq.com>
2021-07-17 09:19:03 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <Home />
+  <router-view></router-view>
 </template>
 
 <script>
-import Home from './views/home/Home.vue'
+
 export default {
   name:'App',
   components:{
-    Home
+    
   }
 }
 </script>
