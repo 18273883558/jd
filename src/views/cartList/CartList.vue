21470d289f873d36e191120883c42e8d5be9e45a
liu yue <1976898266@.com>
2021-08-24 22:06:52 +0800

    购物车页面

--- src/views/cartList/CartList.vue
+++ src/views/cartList/CartList.vue
@@ -1,9 +1,14 @@
 <template>
-  cartList
+  购物车页面
+  <Docker :currentIndex="1"/>
 </template>
 
 <script>
+import Docker from '../../components/Docker.vue'
 export default {
-  
+  name:'CartList',
+  components:{
+    Docker
+  }
 }
 </script>
